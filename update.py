bb319b33d847cba6e9380f4fa857f00d34643ad5
Guillaume Desvé <guillaume.desve@surycat.com>
2016-07-31 16:08:26 +0200

    add addon version logging

--- update.py
+++ update.py
@@ -17,7 +17,7 @@ class Scraper:
             config = json.loads(f.read())
         self.addons_path = config['addons_path']
         self.addons = config['addons']
-        self.noop = config['noop']
+        self.noop = config.get('noop', False)
         if self.noop:
             log.info('NOOP mode')
 
@@ -25,15 +25,16 @@ class Scraper:
         url = 'https://mods.curse.com/addons/wow/{}/download'.format(addon)
         async with session.get(url) as response:
             m = re.search(
-                r'http:\/\/addons\.curse\.cursecdn\.com\/files\/\d+\/\d+\/{}.+\.zip'.format(addon),
+                r'(?P<url>http:\/\/addons\.curse\.cursecdn\.com\/files\/\d+\/\d+\/(?P<version>{}.+)\.zip)'.format(addon),
                 await response.text(),
                 re.IGNORECASE
             )
         if not m:
-            log.error('%s: Download URL not found' % addon)
+            log.error('%s: Addon not found' % addon)
             return
 
-        download_url = m.group(0)
+        download_url = m.group('url')
+        download_version = m.group('version')
         log.info('%s: Downloading from %s', addon, download_url)
         if not self.noop:
             async with session.get(download_url) as response:
@@ -43,7 +44,7 @@ class Scraper:
             z = zipfile.ZipFile(tmp)
             z.extractall(self.addons_path)
         log.info('%s: Extracting to %s', addon, self.addons_path)
-        log.info('%s: Successfully updted', addon)
+        log.info('%s: Successfully updated to version %s', addon, download_version)
 
     async def search(self):
         tasks = []
