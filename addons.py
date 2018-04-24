0b5ed49dad0ad5891e1409ac080bdcd16d2db636
Guillaume Desvé <guillaume.desve@surycat.com>
2018-04-24 16:42:04 +0200

    replace parallelstream with 'halo'

--- addons.py
+++ addons.py
@@ -8,37 +8,7 @@ from io import BytesIO
 from pathlib import Path
 
 from aiohttp import ClientSession
-
-
-class ParallelStreamWriter:
-
-    """
-    Write out messages for operations happening in parallel.
-    Each operation has it's own line, and ANSI code characters are used
-    to jump to the correct line, and write over the line.
-    """
-
-    def __init__(self, msg):
-        self.stream = sys.stdout
-        self.msg = msg
-        self.lines = []
-
-    def initialize(self, obj_index):
-        self.lines.append(obj_index)
-        self.stream.write("{} {} ... \r\n".format(self.msg, obj_index))
-        self.stream.flush()
-
-    def write(self, obj_index, status):
-        position = self.lines.index(obj_index)
-        diff = len(self.lines) - position
-        # move up
-        self.stream.write("%c[%dA" % (27, diff))
-        # erase
-        self.stream.write("%c[2K\r" % 27)
-        self.stream.write("{} {} ... {}\r".format(self.msg, obj_index, status))
-        # move back down
-        self.stream.write("%c[%dB" % (27, diff))
-        self.stream.flush()
+from halo import Halo
 
 
 class Installer:
@@ -54,48 +24,53 @@ class Installer:
         self.addons = config['addons']
         self.noop = noop
         self.session = None
-        self.pwriter = ParallelStreamWriter('Installing')
+
+        # Runtime
+        self.loader = Halo(f'Installing addons... (0/{len(self.addons)})')
+        self._done = []
+        self._failed = []
+
+    def done(self, addon, error=None):
+        if error is not None:
+            self._failed.append((addon, error))
+        else:
+            self._done.append(addon)
+        errors = f', {len(self._failed)} errors' if self._failed else ''
+        self.loader.text = f'Installing addons... ({len(self._done) + len(self._failed)}/{len(self.addons)}{errors})'
 
     async def _alt_install_addon(self, addon):
         """
         Retry on standard Curse website.
         """
         url = f'{self.ALT_CURSE_URL}/wow/addons/{addon}/download'
-        self.pwriter.write(addon, 'trying alternative site')
-
         async with self.session.get(url) as response:
             if response.status != 200:
-                self.pwriter.write(addon, 'not found')
+                self.done(addon, 'not found')
                 return
             match = self.ALT_REGEX.search(await response.text())
 
         if not match:
-            self.pwriter.write(addon, 'regex error (!)')
+            self.done(addon, 'regex error /!\\')
             return
 
         url = f"{self.ALT_CURSE_URL}{match.group('path')}"
-
         async with self.session.get(url) as response:
             if response.status != 200:
-                self.pwriter.write(addon, 'not found')
+                self.done(addon, 'not found')
                 return
             zip_data = await response.read()
 
         if not self.noop:
-            self.pwriter.write(addon, 'extracting')
             z = zipfile.ZipFile(BytesIO(zip_data))
             z.extractall(self.addons_path)
 
-        self.pwriter.write(addon, 'done')
+        self.done(addon)
 
     async def _install_addon(self, addon):
         """
         Install from new Curse project website.
         """
-        self.pwriter.initialize(addon)
         url = f'{self.CURSE_URL}/projects/{addon}/files/latest'
-        self.pwriter.write(addon, 'downloading')
-
         async with self.session.get(url) as response:
             if response.status != 200:
                 await self._alt_install_addon(addon)
@@ -103,19 +78,23 @@ class Installer:
             zip_data = await response.read()
 
         if not self.noop:
-            self.pwriter.write(addon, 'extracting')
             z = zipfile.ZipFile(BytesIO(zip_data))
             z.extractall(self.addons_path)
 
-        self.pwriter.write(addon, 'done')
+        self.done(addon)
 
     async def install(self):
-        tasks = []
+        self.loader.start()
         async with ClientSession() as self.session:
             await asyncio.gather(*[
-                self._install_addon(addon)
-                for addon in self.addons
+                self._install_addon(addon) for addon in self.addons
             ])
+        self.loader.stop()
+
+        for addon, error in self._failed:
+            print(f"Failed to install: '{addon}' ({error})")
+        for addon in self._done:
+            print(f"Successfully installed: '{addon}'")
 
 
 if __name__ == '__main__':
